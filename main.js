a5382279013c73a9eddefd234db1011e5c6de59b
kesslercm <myy535@yahoo.com>
2021-01-23 15:51:26 -0500

    Added event listeners to color divs in grid black

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const container = document.getElementById("container");
 
+//Creates a 16x16 grid
 for (x=0; x<256; x++){
     var newDiv = document.createElement('div');
     newDiv.className = "newDiv";
+    newDiv.setAttribute("id", "newDiv");
     document.getElementById('container').appendChild(newDiv);
 }
+
+//Colors grid black on mouse over
+const gridDiv = document.querySelector('div');
+
+function bgChange(event) {
+    event.target.style.backgroundColor = 'black';
+}
+gridDiv.addEventListener('mouseover', bgChange);
