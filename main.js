8bd89907b128b097482a81d93e8428260f40cec5
kesslercm <myy535@yahoo.com>
2021-02-11 13:56:38 -0500

    Changed CSS and added function to cancel button in prompt

--- main.js
+++ main.js
@@ -24,15 +24,23 @@ for (x=0; x<size * size; x++){
 const gridDiv = document.querySelector('div');
 
 function bgChange(event) {
-    event.target.style.backgroundColor = 'black';
+    event.target.style.backgroundColor = "black";
+    if(parseFloat(event.target.style.opacity)){
+        event.target.style.opacity = parseFloat(event.target.style.opacity) + 0.1;
+    } else {
+        event.target.style.opacity = 0.1;
+    }
 }
+
 gridDiv.addEventListener('mouseover', bgChange);
 
 clear.addEventListener('click', userGridSize);
 
 function userGridSize(){
     let userSize = prompt("Enter a grid size from 1-64");
-    if (userSize < 1 || userSize > 64) {
+    if (userSize === null){
+        return;
+    }else if (userSize < 1 || userSize > 64) {
         alert ("Please enter a number between 1 and 64")
         userGridSize();
     }else {
